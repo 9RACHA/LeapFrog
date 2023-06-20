edb0ccfbe50e484c014e990c1455d274eab04007
9RACHA <66274956+9RACHA@users.noreply.github.com>
2023-06-20 11:46:40 +0200

    Comentarios

--- LeapFrog-20230206/LeapFrog-20230206/Assets/Scripts/GameManager.cs
+++ LeapFrog-20230206/LeapFrog-20230206/Assets/Scripts/GameManager.cs
@@ -3,52 +3,50 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    
     public static GameManager instance;
 
-    private int stepCount = 10;
-    private float stepDistance = 1.8f;
-    
-    private float stepCoordY = -2;
-    private float frogVerticalOffset = 0.5f;
+    private int stepCount = 10; // Número de pasos en el juego
+    private float stepDistance = 1.8f; // Distancia entre cada paso
 
-    public GameObject stepPrefab;
+    private float stepCoordY = -2; // Coordenada Y en la que se generan los pasos
+    private float frogVerticalOffset = 0.5f; // Desplazamiento vertical del personaje "Frog" con respecto a los pasos
 
-    public GameObject frogPrefab;
+    public GameObject stepPrefab; // Prefab del paso
+    public GameObject frogPrefab; // Prefab del personaje "Frog"
 
-    private GameObject frog;
-
+    private GameObject frog; // Referencia al objeto del personaje "Frog"
 
     void Awake() {
         instance = this;
     }
 
     void Start() {
-        SpawnSteps();
-        SpawnFrog();
+        SpawnSteps(); // Generar los pasos en el juego
+        SpawnFrog(); // Aparecer el personaje "Frog"
     }
 
+    // Generar los pasos en el juego
     private void SpawnSteps() {
-
-        for(int i = 0; i < stepCount; i++ ) {
-            float x = CalculateX(i);
-            Instantiate(stepPrefab, new Vector3(x, stepCoordY, 0), Quaternion.identity);
+        for (int i = 0; i < stepCount; i++) {
+            float x = CalculateX(i); // Calcular la posición X del paso actual
+            Instantiate(stepPrefab, new Vector3(x, stepCoordY, 0), Quaternion.identity); // Instanciar el prefab del paso en la posición calculada
         }
-
     }
 
+    // Aparecer el personaje "Frog"
     private void SpawnFrog() {
-        frog = Instantiate(frogPrefab, new Vector3(CalculateX(-1), stepCoordY + frogVerticalOffset, 0), Quaternion.identity);
-        //rana.GetComponent<Rana>().master = this;
+        frog = Instantiate(frogPrefab, new Vector3(CalculateX(-1), stepCoordY + frogVerticalOffset, 0), Quaternion.identity); // Instanciar el prefab del personaje "Frog" en la posición inicial
+        // frog.GetComponent<Frog>().master = this; // Asignar una referencia al objeto "GameManager" en el script del personaje "Frog"
     }
 
+    // Calcular la posición X de un paso dado su índice
     public float CalculateX(int position) {
         float oddEvenCorrection = 0;
-        if(stepCount % 2 == 0) {
-            oddEvenCorrection = stepDistance / 2;
+        if (stepCount % 2 == 0) {
+            oddEvenCorrection = stepDistance / 2; // Corrección para ajustar la posición de los pasos en caso de tener un número par de pasos
         }
 
-        return stepDistance * (position - stepCount/2) + oddEvenCorrection;
+        return stepDistance * (position - stepCount / 2) + oddEvenCorrection; // Calcular la posición X basada en la distancia y el índice del paso
     }
-  
 }
+
